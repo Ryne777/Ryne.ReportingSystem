f96e1cf1344e4aa8fd139385f0d3020114d2b076
Александр Огудов <Ryne777@yandex.ru>
2022-07-06 09:28:06 +0700

    add test

---
+++ Ryne.ReportingSystem.Tests/Infrastructure/TestDefectoscopeApplication.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using Ryne.ReportingSystem.Application.Models;
+using Ryne.ReportingSystem.Application.Service.Interfaces;
+using Ryne.ReportingSystem.Web.Endpoints;
+
+
+namespace Ryne.ReportingSystem.Tests.Infrastructure
+{
+    public class TestDefectoscopeApplication: WebApplicationFactory<DefectoscopeEndpoints>
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            var mock = new Mock<IDefectoscopeService>();
+            mock.Setup(ser => ser.GetList()).Returns(Task.FromResult(TestDefList()));
+            mock.Setup(ser => ser.GetById(Guid.Parse("936DA01F-9ABD-4d9d-80C7-02AF85C822A8"))).Returns(Task.FromResult(OneDefTest()));
+            mock.Setup(ser => ser.CreateOne(OneDefCreateTest())).Returns(Task.FromResult(true));
+            var serviceDescriptor = new ServiceDescriptor(typeof(IDefectoscopeService), mock.Object);
+            builder.ConfigureServices(services =>
+            {
+                services.Replace(serviceDescriptor); ;
+            });
+
+            return base.CreateHost(builder);
+        }
+
+        public static DefectoscopeCreateDTO OneDefCreateTest()
+        {
+            return new DefectoscopeCreateDTO
+            {
+                OrganizationId = Guid.NewGuid(),
+                ProductionYear = 233,
+                SerialNumber = "dff",
+                TypeOfDefectoscopeId = Guid.NewGuid()
+
+            };
+        }
+
+        public static DefectoscopeDetailDTO OneDefTest()
+        {
+            return new DefectoscopeDetailDTO
+            {
+                Id =  Guid.Parse("936DA01F-9ABD-4d9d-80C7-02AF85C822A8"),
+                OrganizationId = Guid.NewGuid(),
+                OrganizationName = "dff",
+                ProductionYear = 23,
+                Repairs = new List<RepairDTO>(),
+                SerialNumber = "df",
+                TypeOfDefectoscopeId = Guid.NewGuid(),
+                TypeOfDefectoscopeName = "er"
+            };
+        }
+
+        public static List<DefectoscopeDTO> TestDefList()
+        {
+            var def = new DefectoscopeDTO
+            {
+                Id = Guid.Parse("936DA01F-9ABD-4d9d-80C7-02AF85C822A8"),
+                OrganizationName = "dfff",
+                ProductionYear = 2022,
+                SerialNumber = "2333",
+                TypeOfDefectoscopeName = "sdff"
+            };
+            return new List<DefectoscopeDTO> {
+                def
+            };
+        }
+    }
+}
--- Ryne.ReportingSystem.Tests/TestDefectoscopeEndpoint.cs
+++ Ryne.ReportingSystem.Tests/TestDefectoscopeEndpoint.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Moq;
 using Ryne.ReportingSystem.Application.Models;
+using Ryne.ReportingSystem.Application.Service.Interfaces;
 using Ryne.ReportingSystem.Web.Endpoints;
 using System.Net;
 using System.Text.Json;
-
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Ryne.ReportingSystem.Tests.Infrastructure;
+using System.Text;
 
 namespace Ryne.ReportingSystem.Tests
 {
@@ -12,7 +17,7 @@ namespace Ryne.ReportingSystem.Tests
         [Fact]
         public async Task TestDefectoscopeGetListEndpointAsync()
         {
-            await using var application = new WebApplicationFactory<DefectoscopeEndpoints>();
+            await using var application = new TestDefectoscopeApplication();
             using var client = application.CreateClient();
 
             var response = await client.GetAsync("/api/defectoscopes");
@@ -22,17 +27,34 @@ namespace Ryne.ReportingSystem.Tests
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.NotNull(returnedDefectoscopes);
         }
+
+
+        [Fact]
         public async Task TestDefectoscopeGetOneEndpointAsync()
         {
-            await using var application = new WebApplicationFactory<DefectoscopeEndpoints>();
+            await using var application = new TestDefectoscopeApplication();
             using var client = application.CreateClient();
-            //TODO: tests
-            var response = await client.GetAsync("/api/defectoscopes/");
+            var response = await client.GetAsync("/api/defectoscopes/936DA01F-9ABD-4d9d-80C7-02AF85C822A8");
             var returnedJson = await response.Content.ReadAsStringAsync();
-            var returnedDefectoscopes = JsonSerializer.Deserialize<List<DefectoscopeDetailDTO>>(returnedJson);
+            var returnedDefectoscopes = JsonSerializer.Deserialize<DefectoscopeDetailDTO>(returnedJson);
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.NotNull(returnedDefectoscopes);
         }
+
+        [Fact]
+        public async Task TestDefectoscopeCreateEndpointAsync()
+        {
+            await using var application = new TestDefectoscopeApplication();
+            using var client = application.CreateClient();
+            var response = await client.PostAsync("/api/defectoscopes/", 
+                new StringContent(
+                    JsonSerializer.Serialize(TestDefectoscopeApplication.OneDefCreateTest()), 
+                    Encoding.UTF32, 
+                    "application/json"));                       
+
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            
+        }
     }
-}
+    }
